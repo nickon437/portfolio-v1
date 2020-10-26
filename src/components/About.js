e6c04471171887a68f520c3261f9d15b355fd895
Nick On <nick.on437@gmail.com>
2020-10-26 22:19:48 +1100

    New - Add about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import Icons from '../assets/values/Icons';
+import { UiContext } from '../context/UiContext';
 
 const About = () => {
+    const [uiContext, setUiContext] = useContext(UiContext);
+    const toolData = [
+        Icons.java,
+        Icons.sceneBuilder,
+        Icons.javascript,
+        Icons.nodeJS, 
+        Icons.reactJS, 
+        Icons.antlr, 
+        Icons.junit, 
+        Icons.jest, 
+        Icons.seleniumWebDriver, 
+        Icons.mySQL, 
+        Icons.appEngine, 
+        Icons.git, 
+    ];
+
+    const toolList = toolData.map((tool) => (
+        <li><img src={tool.logo} alt={tool.alt} /></li>
+    )); 
+
     return (
-        <div>
+        <div id="about" className={`info-card ${uiContext.ctxIsAboutActive ? '' : 'hidden'}`}>
             <div></div>
             <h1>About</h1>
-            <div></div>
-            <div>Technologies I work with</div>
-            
+            <div>A software engineer who loves to collab and build intuitive, maintainable and scalable applications. I have experienced in working in Agiled environments with an emphasis on utilizing best practices to create software that meets requirements and schedule.</div>
+            <h2>Technologies I work with:</h2>
+            <ul>{toolList}</ul>
         </div>
     )
 }
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,12 +1,22 @@
-import React from 'react'
-import yay from '../assets/yay.png';
+import React, { useContext } from 'react'
+import Icons from '../assets/values/Icons'
+import { UiContext } from '../context/UiContext';
 
 export default function Intro() {
+    const [ui, setUi] = useContext(UiContext);
+    
+    const handleClickAbout = () => {
+        setUi((prevUi) => ({
+            ...prevUi,
+            ctxIsAboutActive: true,
+        }));
+    }
+    
     return (
         <div id="intro">
-            <div className="heading">Hi, I am Nick <img src={yay} alt="yay icon"/></div>
-            <div className="content">A front-end developer who wants to bring the optimal experience to <strong>users</strong> through intuitive design and <strong>fellow developers</strong> through clean code.</div>
-            <div className="content">Read more <button type="button" className="link">about me</button> or <button type="button" className="link">contact me</button>.</div>
+            <div className="heading">Hi, I am Nick <img src={Icons.yay.logo} alt={Icons.yay.alt}/></div>
+            <div className="content">A front-end developer who wants to bring the optimal experience with intuitive design, and scalable application.</div>
+            <div className="content">Read more <button type="button" className="link" onClick={handleClickAbout}>about me</button> or <button type="button" className="link">contact me</button>.</div>
         </div>
     )
 }
