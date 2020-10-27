62aebe4ff656f8acea2ed1a8734336095cdeb816
Nick On <nick,on437@gmail.com>
2020-10-27 17:08:48 +1100

    Improve - Animate info pane transition

--- src/components/InfoPane.js
+++ src/components/InfoPane.js
@@ -1,10 +1,10 @@
-import React, { useContext } from 'react'
+import React, { useContext, useRef } from 'react'
 import { UiContext } from '../context/UiContext'
 import About from './About';
 
 const InfoPane = () => {
     const [ui, setUi] = useContext(UiContext);
-    let content = null;
+    const content = useRef(null);
 
     const handleBack = () => {
         setUi((prevUi) => ({
@@ -14,19 +14,17 @@ const InfoPane = () => {
     }
 
     if (ui.ctxInfoPane === 'about') {
-        content = <About />;
-    } else {
-        content = null;
+        content.current = <About />;
     }
 
     return (
-        <div className={ui.ctxInfoPane === '' ? 'hidden' : ''}> 
+        <div className={`info-pane ${ui.ctxInfoPane === '' ? 'info-pane-hidden' : ''}`}> 
             <button className="info-nav-bar" onClick={handleBack}>
                 <svg viewBox="0 0 24 24">
                     <path fill="currentColor" d="M15.41,16.58L10.83,12L15.41,7.41L14,6L8,12L14,18L15.41,16.58Z" />
                 </svg>
             </button>
-            { content }
+            { content.current }
         </div>
     )
 }
