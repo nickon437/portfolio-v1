9fa4a84447bbc718c58ed39a078cdbb435749adc
Nick On <nick,on437@gmail.com>
2020-10-27 17:30:57 +1100

    Improve - Make yay icon shake

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,18 +5,18 @@ import { UiContext } from '../context/UiContext';
 export default function Intro() {
     const [ui, setUi] = useContext(UiContext);
     
-    const handleClickAbout = () => {
+    const handleOpenPane = (e) => {
         setUi((prevUi) => ({
             ...prevUi,
-            ctxInfoPane: 'about',
+            ctxInfoPane: e.target.name,
         }));
     }
-    
+
     return (
         <div id="intro">
-            <div className="heading">Hi, I am Nick <img src={Icons.yay.logo} alt={Icons.yay.alt}/></div>
+            <div className="heading">Hi, I am Nick <img src={Icons.yay.logo} alt={Icons.yay.alt} name={Icons.yay.name}/></div>
             <div className="content">A front-end developer who wants to bring the optimal experience with intuitive design, and scalable application.</div>
-            <div className="content">Read more <button type="button" className="link" onClick={handleClickAbout}>about me</button> or <button type="button" className="link">contact me</button>.</div>
+            <div className="content">Read more <button type="button" className="link" name="about" onClick={handleOpenPane}>about me</button> or <button type="button" className="link" name="contact" onClick={handleOpenPane}>contact me</button>.</div>
         </div>
     )
 }
