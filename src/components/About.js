9028d69bd7bc4d514836d27f0e90e79f6193bf4b
Nick On <nick.on437@gmail.com>
2020-10-27 12:26:08 +1100

    Improve - Componentize info card for reusability

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,7 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import Icons from '../assets/values/Icons';
-import { UiContext } from '../context/UiContext';
 
 const About = () => {
-    const [uiContext, setUiContext] = useContext(UiContext);
     const toolData = [
         Icons.java,
         Icons.sceneBuilder,
@@ -24,8 +22,7 @@ const About = () => {
     )); 
 
     return (
-        <div id="about" className={`info-card ${uiContext.ctxIsAboutActive ? '' : 'hidden'}`}>
-            <div></div>
+        <div id="about" className={`info-card`}>
             <h1>About</h1>
             <div>A software engineer who loves to collab and build intuitive, maintainable and scalable applications. I have experienced in working in Agiled environments with an emphasis on utilizing best practices to create software that meets requirements and schedule.</div>
             <h2>Technologies I work with:</h2>
