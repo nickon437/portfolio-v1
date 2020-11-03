d730eaebb7552ed4343b5769d32e72fd2766b589
Nick On <nick,on437@gmail.com>
2020-11-03 11:10:47 +1100

    New - Add gif demo

---
+++ src/assets/values/Gifs.js
@@ -0,0 +1,17 @@
+import wasp from '../gif/wasp.gif';
+import flocking from '../gif/flocking.gif';
+import datatypes from '../gif/datatypes.gif';
+import debug from '../gif/debug.gif';
+import importIO from '../gif/importIO.gif';
+import sidebar from '../gif/sidebar.gif';
+
+class Gifs {
+    static wasp = { name: 'Wasp', gif: wasp, alt: 'Wasp' };
+    static flocking = { name: 'Flocking', gif: flocking, alt: 'Flocking' };
+    static datatypes = { name: 'Data types', gif: datatypes, alt: 'Data types' };
+    static debug = { name: 'Debug', gif: debug, alt: 'Debug' };
+    static importIO = { name: 'ImportIO', gif: importIO, alt: 'ImportIO' };
+    static sidebar = { name: 'Sidebar', gif: sidebar, alt: 'Sidebar' };
+}
+
+export default Gifs;
